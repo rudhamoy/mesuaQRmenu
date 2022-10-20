0a9e61b36d5a32c39b81fae3e192b81e2d416b4f
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-10-20 07:49:44 +0530

    menu list added

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,20 +1,173 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Collapse } from 'antd';
+const { Panel } = Collapse;
 
 const MenuList = () => {
-  return (
-    <div className="h-[100vh] p-4">
-        <div>
-           <ul className="list-disc leading-10 w-full">
-           <li className="border-b mt-6">Starter</li>
-            <li className="border-b mt-6">Noodles</li>
-            <li className="border-b mt-6">Rice</li>
-            <li className="border-b mt-6">Indian Curry</li>
-            <li className="border-b mt-6">Chinese</li>
-            <li className="border-b mt-6">Beverages</li>
-           </ul>
+    const [showStarter, setShowStarter] = useState(false)
+    const [showNoodles, setShowNoodles] = useState(false)
+    const [showRice, setShowRice] = useState(false)
+    const [showCurry, setShowCurry] = useState(false)
+    const [showChinese, setShowChinese] = useState(false)
+    const [showBev, setShowBevv] = useState(false)
+
+    const onChange = (key) => {
+        console.log(key);
+      };
+
+    return (
+        <div className="h-[100%] p-4">
+                <ul className="list-disc leading-10 w-full">
+                    <li className="border-b mt-6 cursor-pointer" onClick={() => setShowStarter(!showStarter)}>Starter</li>
+                    {showStarter === true && (
+                       <>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Tandoor Chicken</span>
+                            <span>₹ 260/480</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Pork Rib</span>
+                            <span>₹ 330/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Pork Belly</span>
+                            <span>₹ 380/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Shreded Pork with black bean sauce</span>
+                            <span>₹ 200/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Shreded chicken with black bean sauce</span>
+                            <span>₹ 180/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Roasted Bean with pork</span>
+                            <span>₹ 200/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-3">
+                            <span className="col-span-2">Roasted Bean with chicken</span>
+                            <span>₹ 180/-</span>
+                        </li>
+                       </>
+                    )}
+                    <li className="border-b mt-6 cursor-pointer" onClick={() => setShowNoodles(!showNoodles)}>Noodles</li>
+                    {showNoodles === true && (
+                       <>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Veg Noodles</span>
+                            <span>₹ 140/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Chicken Noodles</span>
+                            <span>₹ 180/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Pork Noodles</span>
+                            <span>₹ 200/-</span>
+                        </li>
+                       </>
+                    )}
+                    <li className="border-b mt-6 cursor-pointer" onClick={() => setShowRice(!showRice)}>Rice</li>
+                    {showRice === true && (
+                       <>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Veg</span>
+                            <span>₹ 140/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Chicken</span>
+                            <span>₹ 180/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Pork</span>
+                            <span>₹ 200/-</span>
+                        </li>
+                       </>
+                    )}
+                    <li className="border-b mt-6 cursor-pointer" onClick={() => setShowCurry(!showCurry)}>Indian Curry</li>
+                    {showCurry === true && (
+                       <>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Aloo Matar Panner</span>
+                            <span>₹ 160/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Chicken Kosha</span>
+                            <span>₹ 180/- (5 pieces)</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Pork Kosha</span>
+                            <span>₹ 200/- (5 pieces)</span>
+                        </li>
+                       </>
+                    )}
+                    <li className="border-b mt-6 cursor-pointer" onClick={() => setShowChinese(!showChinese)}>Chinese</li>
+                    {showChinese === true && (
+                       <>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Veg Noodles</span>
+                            <span>₹ 140/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Chicken Noodles</span>
+                            <span>₹ 180/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Pork Noodles</span>
+                            <span>₹ 200/-</span>
+                        </li>
+                       </>
+                    )}
+                    <li className="border-b mt-6 cursor-pointer" onClick={() => setShowBevv(!showBev)}>Beverages</li>
+                    {showBev === true && (
+                       <>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Veg Noodles</span>
+                            <span>₹ 140/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Chicken Noodles</span>
+                            <span>₹ 180/-</span>
+                        </li>
+                        <li className="px-2 grid grid-cols-2">
+                            <span>Pork Noodles</span>
+                            <span>₹ 200/-</span>
+                        </li>
+                       </>
+                    )}
+                </ul>
+                {/* <Collapse accordion className="w-full">
+                    <Panel header="Starter" key="1">
+                        <p>hello</p>
+                    </Panel>
+                    <Panel header="Noodles" key="2">
+                        <ul>
+                            <li>here</li>
+                        </ul>
+                    </Panel>
+                    <Panel header="Rice" key="3">
+                        <ul>
+                            <li>here</li>
+                        </ul>
+                    </Panel>
+                    <Panel header="Indian Curry" key="4">
+                        <ul>
+                            <li>here</li>
+                        </ul>
+                    </Panel>
+                    <Panel header="Chinese" key="5">
+                        <ul>
+                            <li>here</li>
+                        </ul>
+                    </Panel>
+                    <Panel header="Beverages" key="6">
+                        <ul>
+                            <li>here</li>
+                        </ul>
+                    </Panel>
+                </Collapse> */}
         </div>
-    </div>
-  )
+    )
 }
 
 export default MenuList
