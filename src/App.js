b28944151bf073dc5a094d12508a434f7e3fc268
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-10-26 05:07:03 +0530

    updated menu

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ function App() {
       <div className='flex justify-center'>
         <img src="mesua_logo.png" alt="" className="h-[120px]" />
       </div>
-      {/* <div>
-
-      </div> */}
-      <h1 className='my-5 text-xl font-semibold'>Menu List</h1>
-      <MenuList />
+      <div className="my-5 flex flex-col justify-center items-center text-xl font-mono">
+        <p className="font-semibold ">Timing (Mon - Sun)</p>
+        <div className="flex items-center gap-x-4 text-center">
+          <p>11:00 am</p>
+          <span>to</span>
+          <p>10:00 pm</p>
+        </div>
+      </div>
+      <div>
+        <img className="w-[100%]" src="MenuOne.png" alt="" />
+        <img className="w-[100%]" src="MenuTwo.png" alt="" />
+        <img className="w-[100%]" src="MenuThree.png" alt="" />
+      </div>
+      {/* <h1 className='my-5 text-xl font-semibold'>Menu List</h1>
+      <MenuList /> */}
     </div>
   );
 }
